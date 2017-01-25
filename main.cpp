9e69b80b10b13999271787cfe56424d7fe3db98b
otakuto <otakuto893@gmail.com>
2017-01-26 06:41:23 +0900

    support c++11

--- main.cpp
+++ main.cpp
@@ -161,7 +161,7 @@ Health attributeToHealth(Attribute const & attribute)
 
 Health smartToHealth(SMART const & smart)
 {
-	return attributeToHealth(*std::max_element(smart.attribute.cbegin(), smart.attribute.cend(), [](auto lhs, auto rhs)
+	return attributeToHealth(*std::max_element(smart.attribute.cbegin(), smart.attribute.cend(), [](Attribute const & lhs, Attribute const & rhs)
 	{
 		return static_cast<int>(attributeToHealth(lhs)) < static_cast<int>(attributeToHealth(rhs));
 	}));
@@ -426,7 +426,7 @@ int main()
 		}
 		}
 	}
-	std::sort(smartList.begin(), smartList.end(), [](auto lhs, auto rhs){return lhs.deviceName < rhs.deviceName;});
+	std::sort(smartList.begin(), smartList.end(), [](SMART const & lhs, SMART const & rhs){return lhs.deviceName < rhs.deviceName;});
 
 	if (smartList.size() == 0)
 	{
